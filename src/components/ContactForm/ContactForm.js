0984240eebd273aa0e41ed1134d4946da164db83
Ben Gallard <bengallard@gmail.com>
2019-12-23 17:13:28 +0000

    Updated contact form styles

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -4,26 +4,17 @@ import classes from './ContactForm.module.css'
 
 const ContactForm = (props) => (
     <div className={classes.ContactForm}>
-                <form>
-                    <h3>Contact Us</h3>
-                    <div>
-                        <div>Name*</div>
-                        <input type='text' />
-                    </div>
-                    <div>
-                        <div>Email*</div>
-                        <input type='text' />
-                    </div>
-                    <div>
-                        <p></p>
-                        <div>Message*</div>
-                        <input type='text' />
-                    </div>
-                    <p></p>
-                    <button>Submit</button>
-                    <p></p>
-                </form>
-            </div>
+            <h3>Contact Us</h3>
+            <form>
+                <div>Name*</div>
+                <input type='text' required={true} />
+                <div>Email*</div>
+                <input type='email' required={true} />
+                <div>Message*</div>
+                <input type='text' required={true} multiple={true} />
+                <button>Submit</button>
+            </form>
+        </div>
 )
 
 export default ContactForm
