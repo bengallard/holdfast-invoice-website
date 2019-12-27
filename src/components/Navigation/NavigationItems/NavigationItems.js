9fca8e34a99d2e917a3ba857143fdf8c1351c4e5
Ben Gallard <bengallard@gmail.com>
2019-12-27 22:29:58 +0000

    Remove app store button

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,18 +2,12 @@ import React from 'react'
 
 import classes from './NavigationItems.module.css'
 import NavigationItem from './NavigationItem/NavigationItem'
-import AppStoreButton from '../../AppStoreButton/AppStoreButton'
 
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" active>Home</NavigationItem>
         <NavigationItem link="/">Our Story</NavigationItem>
         <NavigationItem link="/">Contact Us</NavigationItem>
-        <NavigationItem link="https://apps.apple.com/app/holdfast-invoice/id1478543802">
-            <div className={classes.AppStoreButton}>
-                <AppStoreButton />
-            </div>
-        </NavigationItem>
     </ul>
 )
 
