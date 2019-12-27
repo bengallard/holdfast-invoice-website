2418e0beee0746c14295716d31f1ebbafd36b1cd
Ben Gallard <bengallard@gmail.com>
2019-12-27 22:27:06 +0000

    App store image wrapped in an anchor

--- src/components/AppStoreButton/AppStoreButton.js
+++ src/components/AppStoreButton/AppStoreButton.js
@@ -4,9 +4,9 @@ import classes from './AppStoreButton.module.css'
 import appStoreImage from '../../assets/Download_on_the_App_Store_Badge_US-UK_blk_092917.png'
 
 const appStoreButton = (props) => (
-    <div className={classes.AppStoreButton}>
-        <img src={appStoreImage} alt="iOS App Store" />
-    </div>
+        <a className={classes.AppStoreButton} href={props.link}>
+            <img src={appStoreImage} alt="iOS App Store" />
+        </a>
 )
 
 export default appStoreButton
