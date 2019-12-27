1cd9eec4d6f2f8bbc2d591b435c8bd0e2600cf6f
Ben Gallard <bengallard@gmail.com>
2019-12-27 22:28:34 +0000

    Footer items have dummy links

--- src/components/Navigation/FooterItems/FooterItems.js
+++ src/components/Navigation/FooterItems/FooterItems.js
@@ -5,10 +5,10 @@ import FooterItem from './FooterItem/FooterItem'
 
 const footerItems = (props) => (
     <ul className={classes.FooterItems}>
-        <FooterItem>Home</FooterItem>
-        <FooterItem>Our Story</FooterItem>
-        <FooterItem>Contact Us</FooterItem>
-        <FooterItem>Privacy Policy</FooterItem>
+        <FooterItem link="/">Home</FooterItem>
+        <FooterItem link="/">Our Story</FooterItem>
+        <FooterItem link="/">Contact Us</FooterItem>
+        <FooterItem link="/">Privacy Policy</FooterItem>
     </ul>
 )
 
