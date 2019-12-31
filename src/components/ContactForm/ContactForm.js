d88e8a9a7aeabd34564283dc18db65480f894783
Ben Gallard <bengallard@gmail.com>
2019-12-31 09:26:40 +0000

    Updated UI

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,19 +2,31 @@ import React from 'react'
 
 import classes from './ContactForm.module.css'
 
-const ContactForm = (props) => (
-    <div className={classes.ContactForm}>
-            <h3>Contact Us</h3>
-            <form>
-                <div>Name*</div>
-                <input type='text' required={true} />
-                <div>Email*</div>
-                <input type='email' required={true} />
-                <div>Message*</div>
-                <input type='text' required={true} multiple={true} />
-                <button>Submit</button>
-            </form>
-        </div>
-)
+const ContactForm = (props) => {
+    const clickedHandler = (event) => {
+
+    }
+
+    let form = <div className={classes.ContactForm}>
+        <h2>Contact Us</h2>
+        <form>
+            <h4>Name*</h4>
+            <input type='text' required={true} />
+            <h4>Email*</h4>
+            <input type='email' required={true} />
+            <h4>Message*</h4>
+            <input type='text' required={true} multiple={true} />
+            <button onClick={clickedHandler}>Submit</button>
+        </form>
+    </div>
+
+    let sent = props.sent
+    if (sent) {
+        form = <p style={{textAlign: 'center'}}>Thank you for your message. We have recieved it and will respond within 24hrs.</p>
+    }
+
+    return form
+    
+}
 
 export default ContactForm
