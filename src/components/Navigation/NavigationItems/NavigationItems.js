230635eab61638dc3e85be4d6052af456377f3e3
Ben Gallard <bengallard@gmail.com>
2019-12-31 09:27:42 +0000

    Remove div wrapping element

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,12 +4,10 @@ import classes from './NavigationItems.module.css'
 import NavigationItem from './NavigationItem/NavigationItem'
 
 const navigationItems = (props) => (
-    <div className={classes.NavigationItems}>
-        <ul>
+    <ul className={classes.NavigationItems}>
             <NavigationItem link="/about-us">Our Story</NavigationItem>
             <NavigationItem link="/contact-us">Contact Us</NavigationItem>
-        </ul>
-    </div>
+    </ul>
     
 )
 
