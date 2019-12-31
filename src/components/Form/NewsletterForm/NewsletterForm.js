bf407e5f3a7707b11bbf7cd1050db2964721b82f
Ben Gallard <bengallard@gmail.com>
2019-12-31 18:22:19 +0000

    Added Newsletter component

---
+++ src/components/Form/NewsletterForm/NewsletterForm.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import classes from './NewsletterForm.module.css'
+import Form from '../Form/Form'
+import FormItem from '../Form/FormItem/FormItem'
+
+const NewsletterForm = (props) => {
+    let formItems = [
+        <FormItem inputType="input" type="text" title="Name" required />,
+        <FormItem inputType="input" type="email" title="Email" required />,
+    ]
+    const title = "Want to be in the know?"
+    const subtitle = "Sign-up to our newsletter and recieve occasional updates and tips!"
+
+    const submitMsgTitle = "Thanks for signing up!"
+    const submitMsg = "Please check your email for confirmation."
+
+    return (
+        <div className={classes.NewsletterForm}>
+            <Form 
+                title={title} 
+                subtitle={subtitle}
+                items={formItems} 
+                buttonText="Sign me up!"
+                submitMsgTitle={submitMsgTitle}
+                submitMsg={submitMsg}
+                submitted={false} />
+        </div>
+        
+    )
+}
+
+export default NewsletterForm
