31139bd7ff650f0a908b28a9a4f9995ed3860ae8
Ben Gallard <bengallard@gmail.com>
2019-12-31 09:24:11 +0000

    Add Routing to pages: - privacy policy - page not found

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Layout from './hoc/Layout/Layout'
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
+import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy'
 
 function App() {
   return (
     <Layout>
-      <Route path='/about-us' exact component={About} />
-      <Route path='/contact-us' exact component={Contact} />
-      <Route path='/' exact component={Home} />
+      <Switch>
+        <Route path='/about-us' exact component={About} />
+        <Route path='/contact-us' exact component={Contact} />
+        <Route path='/privacy-policy' exact component={PrivacyPolicy} />
+        <Route path='/' exact component={Home} />
+        <Route render={() => <h1 style={{textAlign: 'center'}}>Page not found</h1>} />
+      </Switch>
     </Layout>
   )
 }
