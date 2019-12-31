bcf244611f2766dcc58fd068f48f32dcfd8dda85
Ben Gallard <bengallard@gmail.com>
2019-12-31 09:25:10 +0000

    Add styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,23 @@
 import React from 'react'
 
+import classes from './About.module.css'
 import FooterMenu from '../Navigation/FooterMenu/FooterMenu'
 
 const about = (props) => (
     <div>
-        <h2>Client Appointment Invoicing Software for iOS</h2>
-        <p>Holdfast Invoice is optimised for your appointment and group-client based small business.</p>
-        <p>Designed specifically for Personal Trainers, Yoga Instructors, Massage Therapists and any appointment based businesses.</p>
-        <p>We've created an app that takes care of your invoicing for you, utilising a simple, no fuss user interface making invoicing fast.</p>
-        <p>Tracking your invoices is fast and easy with built-in email and invoice management.</p>
-        
+        <div className={classes.About}>
+            <h2>Designed specifically for fitness professionals</h2>
+            <p>Intended to be the quickest invoicing solution for client based appointment businesses, Holdfast Invoice was designed by fitness professionals for fitness professionals.</p>
+            <p>Holdfast Invoice is optimised for your appointment and group-client based small business.</p>
+            <p>Designed specifically for Personal Trainers, Yoga Instructors, Massage Therapists and any appointment based businesses.</p>
+            <p>We've created an app that takes care of your invoicing for you, utilising a simple, no fuss user interface making invoicing fast.</p>
+            <p>Tracking your invoices is fast and easy with built-in email and invoice management.</p>
+            
+            
+        </div>
         <FooterMenu />
     </div>
+    
 )
 
 export default about
