7f4de3ff6d7c938363baa9c971854f632eb54f91
Ben Gallard <bengallard@gmail.com>
2020-01-02 20:49:26 +0000

    Added state to form

--- src/components/Form/ContactForm/ContactForm.js
+++ src/components/Form/ContactForm/ContactForm.js
@@ -1,35 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import classes from './ContactForm.module.css'
 import Form from '../Form/Form'
 import FormItem from '../Form/FormInput/FormInput'
 
-const contactForm = (props) => {
-    let formItems = [
-        <FormItem elementType="input" type="text" title="Name" required />,
-        <FormItem elementType="input" type="email" title="Email" required />,
-        <FormItem elementType="textarea" type="text" title="Message" required />
-    ]
-    const title = "Have a question?"
-    const subtitle = "Let us know how we're doing! Feel free to leave your feedback or ask a question here."
+class ContactForm extends Component {
+    state = {
+        title: "Have a question?",
+        subtitle: "Let us know how we're doing! Feel free to leave your feedback or ask a question here.",
+        submitMsgTitle: "Thank you for getting in touch",
+        submitMsg: "We have recieved your message and will respond within 24hrs.",
+        buttonText: "Send",
+        formItems: [
+            <FormItem elementType="input" type="text" title="Name" required />,
+            <FormItem elementType="input" type="email" title="Email" required />,
+            <FormItem elementType="textarea" type="text" title="Message" required />
+        ],
+        loading: false,
+        submitted: false
+    }
 
-    const submitMsgTitle = "Thank you for getting in touch"
-    const submitMsg = "We have recieved your message and will respond within 24hrs."
-    
+    submitHandler = (event) => {
+        event.preventDefault()
+        this.setState({loading: true})
+    }
 
-    return (
-        <div className={classes.ContactForm}>
-            <Form 
-                title={title} 
-                subtitle={subtitle}
-                items={formItems} 
-                buttonText="Send"
-                submitMsgTitle={submitMsgTitle}
-                submitMsg={submitMsg}
-                submitted={false} />
-        </div>
-        
-    )
+    render() {
+        return (
+            <div className={classes.ContactForm}>
+                <Form 
+                    submitSelected={this.submitHandler}
+                    title={this.state.title} 
+                    subtitle={this.state.subtitle}
+                    items={this.state.formItems} 
+                    buttonText={this.state.buttonText}
+                    submitMsgTitle={this.state.submitMsgTitle}
+                    submitMsg={this.state.submitMsg}
+                    submitted={this.state.submitted}
+                    loading={this.state.loading} />
+            </div>
+        )
+    }
 }
 
-export default contactForm
+export default ContactForm
--- src/components/Form/NewsletterForm/NewsletterForm.js
+++ src/components/Form/NewsletterForm/NewsletterForm.js
@@ -1,33 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import classes from './NewsletterForm.module.css'
 import Form from '../Form/Form'
 import FormItem from '../Form/FormInput/FormInput'
 
-const NewsletterForm = (props) => {
-    let formItems = [
-        <FormItem key="name" elementType="input" type="text" title="Name" required />,
-        <FormItem key="email" elementType="input" type="email" title="Email" required />,
-    ]
-    const title = "Want to stay in the know?"
-    const subtitle = "Sign-up to our newsletter and recieve occasional updates and tips!"
+class NewsletterForm extends Component {
+    state = {
+        title: "Want to stay in the know?",
+        subtitle: "Sign-up to our newsletter and recieve occasional updates and tips!",
+        submitMsgTitle: "Thanks for signing up!",
+        submitMsg: "Please check your email for confirmation.",
+        buttonText: "Sign me up!",
+        formItems: [
+            <FormItem key="name" elementType="input" type="text" title="Name" required />,
+            <FormItem key="email" elementType="input" type="email" title="Email" required />,
+        ],
+        loading: false,
+        submitted: false
+    }
 
-    const submitMsgTitle = "Thanks for signing up!"
-    const submitMsg = "Please check your email for confirmation."
-
-    return (
-        <div className={classes.NewsletterForm}>
-            <Form 
-                title={title} 
-                subtitle={subtitle}
-                items={formItems} 
-                buttonText="Sign me up!"
-                submitMsgTitle={submitMsgTitle}
-                submitMsg={submitMsg}
-                submitted={false} />
-        </div>
-        
-    )
+    submitHandler = (event) => {
+        event.preventDefault()
+        this.setState({loading: true})
+    }
+    
+    render() {
+        return (
+            <div className={classes.NewsletterForm}>
+                <Form 
+                    submitSelected={this.submitHandler}
+                    title={this.state.title} 
+                    subtitle={this.state.subtitle}
+                    items={this.state.formItems} 
+                    buttonText={this.state.buttonText}
+                    submitMsgTitle={this.state.submitMsgTitle}
+                    submitMsg={this.state.submitMsg}
+                    submitted={this.state.submitted} 
+                    loading={this.state.loading}/>
+            </div>
+        )
+    }
 }
 
 export default NewsletterForm
