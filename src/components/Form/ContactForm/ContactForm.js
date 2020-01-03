dbb5b68aa5b203df6c5d4a098d984ebba90bc98b
Ben Gallard <bengallard@gmail.com>
2020-01-03 13:50:22 +0000

    Add contact message to firebase

--- src/components/Form/ContactForm/ContactForm.js
+++ src/components/Form/ContactForm/ContactForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import classes from './ContactForm.module.css'
 import Form from '../Form/Form'
-import FormItem from '../Form/FormInput/FormInput'
+import axios from '../../../axios-message'
 
 class ContactForm extends Component {
     state = {
@@ -11,33 +11,87 @@ class ContactForm extends Component {
         submitMsgTitle: "Thank you for getting in touch",
         submitMsg: "We have recieved your message and will respond within 24hrs.",
         buttonText: "Send",
-        formItems: [
-            <FormItem elementType="input" type="text" title="Name" required />,
-            <FormItem elementType="input" type="email" title="Email" required />,
-            <FormItem elementType="textarea" type="text" title="Message" required />
-        ],
+        formInputs: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    title: 'Name',
+                    placeholder: '',
+                    required: true
+                },
+                value: '',
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    title: 'Email',
+                    placeholder: '',
+                    required: true
+                },
+                value: '',
+            },
+            message: {
+                elementType: 'textarea',
+                elementConfig: {
+                    type: 'text',
+                    title: 'Message',
+                    placeholder: '',
+                    required: true
+                },
+                value: '',
+            }
+        },
         loading: false,
         submitted: false
     }
 
-    submitHandler = (event) => {
+    submitFormHandler = (event) => {
         event.preventDefault()
         this.setState({loading: true})
+        const customerSignupData = {}
+        for (let formInputId in this.state.formInputs) {
+            customerSignupData[formInputId] = this.state.formInputs[formInputId].value
+        }
+        console.log(customerSignupData)
+        axios.post('/customer-messages.json', customerSignupData)
+            .then(response => {
+                console.log('success')
+                this.setState(
+                    {loading: false, submitted: true}
+                )
+            })
+            .catch(error => {
+                console.log('failed:', error)
+                this.setState(
+                    {loading: false, submitted: false}
+                )
+            })
+    }
+
+    inputChangedHandler = (event, inputId) => {
+        const updatedFormInputs = {...this.state.formInputs}
+        const updatedFormItem = {...updatedFormInputs[inputId]}
+        updatedFormItem.value = event.target.value
+        updatedFormInputs[inputId] = updatedFormItem
+        this.setState({ formInputs: updatedFormInputs })
     }
 
     render() {
         return (
             <div className={classes.ContactForm}>
                 <Form 
-                    submitSelected={this.submitHandler}
+                    submitSelected={this.submitFormHandler}
                     title={this.state.title} 
                     subtitle={this.state.subtitle}
-                    items={this.state.formItems} 
+                    items={this.state.formInputs} 
                     buttonText={this.state.buttonText}
                     submitMsgTitle={this.state.submitMsgTitle}
                     submitMsg={this.state.submitMsg}
                     submitted={this.state.submitted}
-                    loading={this.state.loading} />
+                    loading={this.state.loading}
+                    inputChanged={(event, inputId) => this.inputChangedHandler(event, inputId)} />
             </div>
         )
     }
