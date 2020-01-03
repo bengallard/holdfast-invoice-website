fcb98302e5eb5c289aa04752bce288882235b1cd
Ben Gallard <bengallard@gmail.com>
2020-01-03 13:52:47 +0000

    Create FormItem(s) from items prop

--- src/components/Form/Form/Form.js
+++ src/components/Form/Form/Form.js
@@ -1,12 +1,30 @@
 import React from 'react'
 
 import classes from './Form.module.css'
+import FormInput from './FormInput/FormInput'
 
 const Form = (props) => {
+    let formItems = []
+    for (let key in props.items) {
+        formItems.push({
+            id: key,
+            elementType: props.items[key].elementType,
+            config: props.items[key].elementConfig
+        })
+    }
     let form = <form className={classes.Form} onSubmit={props.submitSelected}>
         <h2>{props.title}</h2>
         <h4>{props.subtitle}</h4>
-        {props.items.map(formItem => formItem)}
+        {formItems.map(formItem => {
+            return (
+                <FormInput 
+                    key={formItem.id}
+                    elementType={formItem.elementType}
+                    config={formItem.config}
+                    value={formItem.value}
+                    changed={(event) => props.inputChanged(event, formItem.id)} />
+            )
+        } )}
         <button>{(props.buttonText) ? props.buttonText : "Submit"}</button>
     </form>
 
