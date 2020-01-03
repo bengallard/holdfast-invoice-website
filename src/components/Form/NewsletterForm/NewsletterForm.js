7617500ae66aec348ef1c91d60078bac0d938363
Ben Gallard <bengallard@gmail.com>
2020-01-03 13:54:22 +0000

    Handle input changes Form sends to database pass formInputs as dict prop

--- src/components/Form/NewsletterForm/NewsletterForm.js
+++ src/components/Form/NewsletterForm/NewsletterForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import classes from './NewsletterForm.module.css'
 import Form from '../Form/Form'
-import FormItem from '../Form/FormInput/FormInput'
+import axios from '../../../axios-sign-up'
 
 class NewsletterForm extends Component {
     state = {
@@ -11,32 +11,76 @@ class NewsletterForm extends Component {
         submitMsgTitle: "Thanks for signing up!",
         submitMsg: "Please check your email for confirmation.",
         buttonText: "Sign me up!",
-        formItems: [
-            <FormItem key="name" elementType="input" type="text" title="Name" required />,
-            <FormItem key="email" elementType="input" type="email" title="Email" required />,
-        ],
+        formInputs: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    title: 'Name',
+                    placeholder: '',
+                    required: true
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    title: 'Email',
+                    placeholder: '',
+                    required: true
+                },
+                value: ''
+            }
+        },
         loading: false,
         submitted: false
     }
 
-    submitHandler = (event) => {
+    submitFormHandler = (event) => {
         event.preventDefault()
         this.setState({loading: true})
+        const customerSignupData = {}
+        for (let formInputId in this.state.formInputs) {
+            customerSignupData[formInputId] = this.state.formInputs[formInputId].value
+        }
+        axios.post('/newsletter-signup.json', customerSignupData)
+            .then(response => {
+                console.log('success')
+                this.setState(
+                    {loading: false, submitted: true}
+                )
+            })
+            .catch(error => {
+                console.log('failed:', error)
+                this.setState(
+                    {loading: false, submitted: false}
+                )
+            })
+    }
+
+    inputChangedHandler = (event, inputId) => {
+        const updatedFormInputs = {...this.state.formInputs}
+        const updatedFormItem = {...updatedFormInputs[inputId]}
+        updatedFormItem.value = event.target.value
+        updatedFormInputs[inputId] = updatedFormItem
+        this.setState({ formInputs: updatedFormInputs })
     }
     
     render() {
         return (
             <div className={classes.NewsletterForm}>
                 <Form 
-                    submitSelected={this.submitHandler}
+                    submitSelected={this.submitFormHandler}
                     title={this.state.title} 
                     subtitle={this.state.subtitle}
-                    items={this.state.formItems} 
+                    items={this.state.formInputs} 
                     buttonText={this.state.buttonText}
                     submitMsgTitle={this.state.submitMsgTitle}
                     submitMsg={this.state.submitMsg}
                     submitted={this.state.submitted} 
-                    loading={this.state.loading}/>
+                    loading={this.state.loading}
+                    inputChanged={(event, inputId) => this.inputChangedHandler(event, inputId)} />
             </div>
         )
     }
